f32f661b1c885694e9d6610e7d6bf736269a6b2c
Roger Chen <roger.qi.chen@gmail.com>
2024-10-04 10:38:41 +1000

    add aks

---
+++ azure/tf-modules/aks/output.tf
@@ -0,0 +1,23 @@
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+# output "p_acr_id" {
+#   value = azurerm_container_registry.acr_product.id
+# }
+
+# output "s_acr_id" {
+#   value = azurerm_container_registry.acr_store.id
+# }
+
+# output "acr_login_server" {
+#   value = azurerm_container_registry.acr.login_server
+# }

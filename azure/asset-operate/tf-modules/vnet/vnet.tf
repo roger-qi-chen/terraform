76e38a261b8a57fe9838e54e5daa2466bee18e5a
Roger Chen <rogerchen@assetfuture.com>
2025-02-22 12:40:12 +1100

    bug fix

--- azure/asset-operate/tf-modules/vnet/vnet.tf
+++ azure/asset-operate/tf-modules/vnet/vnet.tf
@@ -1,7 +1,7 @@
 # Create virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
-  location            = var.resource_group_name
-  resource_group_name = var.resource_group_location
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   address_space       = var.address_space
 }
